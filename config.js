a4acc1db072183d7a46b4ac6404d77d154478f41
Antz72 <antonybeernink@gmail.com>
2025-06-26 11:45:50 +1200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+    YOUTUBE_CHANNEL_ID: 'UC09QwXpdgjgd6l8BFBRlZMw',
+    STATIC_HASHTAGS: ['#Gaming', '#PCGaming', '#NewVideo'],
+    MAX_YOUTUBE_RESULTS: 3, // Make the number of results to fetch configurable
+};
